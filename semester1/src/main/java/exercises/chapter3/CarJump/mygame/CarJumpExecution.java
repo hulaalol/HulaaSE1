938a01bd1f850161173c25dbc0724a70758fb614
Marci Hulaa <marci.vfb4ever@googlemail.com>
2015-04-24 17:28:14 +0200

    removed unnecessary comments in CarJump

--- semester1/src/main/java/exercises/chapter3/CarJump/mygame/CarJumpExecution.java
+++ semester1/src/main/java/exercises/chapter3/CarJump/mygame/CarJumpExecution.java
@@ -6,23 +6,23 @@ public class CarJumpExecution {
 
 	public static void main(String[] args) {
 		
-		final Scanner scan = new Scanner(System.in);   // Read user input: User decides of how many lines the tree exists
+		final Scanner scan = new Scanner(System.in);   
 		System.out.print("Gewicht Auto in kg=");
 		final int masseAutoUserInput = scan.nextInt();
 		
-		final Scanner scan2 = new Scanner(System.in);   // Read user input: User decides of how many lines the tree exists
+		final Scanner scan2 = new Scanner(System.in);   
 		System.out.print("Höhe der Startrampe in Meter=");
 		final int hoeheStartrampeUserInput = scan2.nextInt();
 		
-		final Scanner scan3 = new Scanner(System.in);   // Read user input: User decides of how many lines the tree exists
+		final Scanner scan3 = new Scanner(System.in);   
 		System.out.print("Winkel der Startrampe in °=");
 		final int winkelStartrampeUserInput = scan3.nextInt();
 		
-		final Scanner scan4 = new Scanner(System.in);   // Read user input: User decides of how many lines the tree exists
+		final Scanner scan4 = new Scanner(System.in);   
 		System.out.print("Höhe der Absprungrampe=");
 		final int hoeheRampeUserInput = scan4.nextInt();
 		
-		final Scanner scan5 = new Scanner(System.in);   // Read user input: User decides of how many lines the tree exists
+		final Scanner scan5 = new Scanner(System.in);   
 		System.out.print("Winkel der Absprungrampe=");
 		final int winkelRampeUserInput = scan5.nextInt();
 		
