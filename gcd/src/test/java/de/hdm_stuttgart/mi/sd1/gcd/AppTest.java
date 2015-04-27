784dba9b7721aa1095e7678812c01fd0cf30d166
mh252 <mh252@localhost>
2015-04-27 12:02:01 +0200

    Initiale Projekterstellung

---
+++ gcd/src/test/java/de/hdm_stuttgart/mi/sd1/gcd/AppTest.java
@@ -0,0 +1,17 @@
+package de.hdm_stuttgart.mi.sd1.gcd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Dummy test method
+     */
+    @Test
+    public void testApp() {
+        Assert.assertTrue( true );
+    }
+}
