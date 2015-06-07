6a45d9b429d6f88e432c9fdd033184173b31e76c
hulaalol <marci.vfb4ever@googlemail.com>
2015-06-07 18:29:01 +0200

    implementation of exp-function

---
+++ Math/src/main/java/SE1/Math/Execution.java
@@ -0,0 +1,23 @@
+package SE1.Math;
+import SE1.Math.Math;
+public class Execution {
+
+	public static void main(String[] args) {
+		
+		double a = 8.5;
+		double b = 8.7;
+		double c = -9.4;
+		
+//		System.out.println(Math.max(a,b));
+//		System.out.println(Math.abs(c));
+//		System.out.println(Math.max(a,b,c));
+		
+		
+		
+		for (int i = -5; i < 10; i++)
+		{
+		System.out.println(Math.exp(i));
+		}
+	}
+
+}
