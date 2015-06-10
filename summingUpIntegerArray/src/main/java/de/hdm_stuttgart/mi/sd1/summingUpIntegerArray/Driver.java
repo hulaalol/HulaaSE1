546099cfdba4255fd080f5e1a96c92d4f10d80e8
mh252 <mh252@localhost>
2015-06-10 15:52:30 +0200

    komische aufgabe... alles zerbombt :D

--- summingUpIntegerArray/src/main/java/de/hdm_stuttgart/mi/sd1/summingUpIntegerArray/Driver.java
+++ summingUpIntegerArray/src/main/java/de/hdm_stuttgart/mi/sd1/summingUpIntegerArray/Driver.java
@@ -6,11 +6,12 @@ public class Driver {
 
 		final Scanner scanner = new Scanner(System.in);
 
-		System.out.print("How many values do you want to summ up? ");
+		System.out.print("How many values do you want to sum up? ");
 		final int numValues = scanner.nextInt();
 
 		int sum = 0;
 		int valueArray[] = new int[numValues];
+		
 
 		for (int i = 0; i < numValues; i++) {
 			System.out.print("Enter value #" + (i + 1) + " of " + numValues + ": ");
@@ -18,27 +19,76 @@ public class Driver {
 			valueArray[i]= userInput;
 			sum += userInput;
 		}
+		
 
-		for (int i = 0; i<(valueArray.length); i++)
+		int faggit[] = new int[killZeros.killZerosArray(valueArray)];	
+		
+		for (int i=0; i<valueArray.length;i++) //todo!
 		{
-			
-			if (valueArray[i]>=0)
+			if (valueArray[i]==0)
 			{
 				
-				if (i != 0)
+			}else {
+				for (int k=0; k<2; k++)
 				{
-					System.out.print("+");
+				faggit[k] = valueArray[i];
 				}
-				System.out.print(valueArray[i]);
-				
-			} else {
-				
-				System.out.print(valueArray[i]);
-				
+			}
+		}				
+
+		
+		for (int i = 0; i < faggit.length; i++) {
+			System.out.print(faggit[i]);
+		}									//todo
+		
+		
+		
+
+		if (valueArray.length==1)
+		{
+			System.out.print(valueArray[0]);
+		}else 
+		{
+
+
+			for (int i = 0; i<(valueArray.length); i++)
+			{
+
+				if (valueArray[i]==0)
+				{
+					; //chill
+				}
+
+				if (valueArray[i]>0)
+				{
+					
+					
+					if (valueArray[i+1]==0)
+					{
+						System.out.print(valueArray[i]);
+					} else if (i != 0)
+					{
+						System.out.print("+");
+						System.out.print(valueArray[i]);
+					}
+
+				} else if (valueArray[i]<0){
+
+					System.out.print(valueArray[i]);
+
+				}
+
 			}
 
+
+			if (sum==0)
+			{
+				System.out.print("0");
+			} else
+			{
+				System.out.print(" = " + sum);
+				scanner.close();
+			}
 		}
-		System.out.print(" = " + sum);
-		scanner.close();
 	}
 }
---
+++ summingUpIntegerArray/src/main/java/de/hdm_stuttgart/mi/sd1/summingUpIntegerArray/killZeros.java
@@ -0,0 +1,33 @@
+package de.hdm_stuttgart.mi.sd1.summingUpIntegerArray;
+
+public class killZeros {
+	
+	public static int killZerosArray(int a[])
+	{
+		int numberOfZeros = 0;
+		
+		for (int i = 0; i<a.length; i++)
+		{
+			if (a[i]==0)
+			{
+				numberOfZeros++;
+			}else
+			{
+				
+			}
+			
+//			if ((i-1)==a.length)
+//			{
+//				return numberOfZeros;
+//			}
+	}	
+		return numberOfZeros;
+}
+}
+
+
+
+	
+	
+	
+
