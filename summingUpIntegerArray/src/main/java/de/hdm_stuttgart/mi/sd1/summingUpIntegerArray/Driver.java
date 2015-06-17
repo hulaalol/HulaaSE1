1c09d465df47e2f6123ed04d91e8e59f7d27cfa1
mh252 <mh252@localhost>
2015-06-17 15:40:03 +0200

    fixed killZeros method implementation... works lelelle

--- summingUpIntegerArray/src/main/java/de/hdm_stuttgart/mi/sd1/summingUpIntegerArray/Driver.java
+++ summingUpIntegerArray/src/main/java/de/hdm_stuttgart/mi/sd1/summingUpIntegerArray/Driver.java
@@ -1,9 +1,11 @@
 package de.hdm_stuttgart.mi.sd1.summingUpIntegerArray;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Driver {
 	public static void main(String[] args) {
 
+		//get user Input about number of values being summed up and the values itself.
 		final Scanner scanner = new Scanner(System.in);
 
 		System.out.print("How many values do you want to sum up? ");
@@ -20,75 +22,58 @@ public class Driver {
 			sum += userInput;
 		}
 		
-
-		int faggit[] = new int[killZeros.killZerosArray(valueArray)];	
 		
-		for (int i=0; i<valueArray.length;i++) //todo!
+		// create new arrayWithoutZeros having the legnth of valueArray except all Zeros.
+		int arrayWithoutZeros[] = new int[valueArray.length - killZeros.killZerosArray(valueArray)];	
+		// zeros get killed by killZerosArray-Method.
+		
+		
+		// assign the non-Zero values of valueArray to the new arrayWithoutZeros
+		int k=0;
+		
+		for (int i=0; i<valueArray.length;i++)
 		{
+			
 			if (valueArray[i]==0)
 			{
 				
 			}else {
-				for (int k=0; k<2; k++)
-				{
-				faggit[k] = valueArray[i];
-				}
+				arrayWithoutZeros[k] = valueArray[i];
+				k++;
 			}
 		}				
 
 		
-		for (int i = 0; i < faggit.length; i++) {
-			System.out.print(faggit[i]);
-		}									//todo
 		
+		//sort the arrayWithoutZeros in ascending order
+		Arrays.sort(arrayWithoutZeros);
 		
+		// reverse order of arrayWithoutZeros in new arrayFinal to achieve descending order
+		int arrayFinal[] = new int[arrayWithoutZeros.length];
 		
-
-		if (valueArray.length==1)
+		int z=0;
+		for (int i = arrayWithoutZeros.length; i > 0; i--)
 		{
-			System.out.print(valueArray[0]);
-		}else 
-		{
-
-
-			for (int i = 0; i<(valueArray.length); i++)
+			
+			arrayFinal[i-1] = arrayWithoutZeros[z];
+			z++;
+		}
+		
+		//print index 0 of arrayFinal to Console without sign (+/-)
+		System.out.print(arrayFinal[0]);
+		//print values of arrayFinal to Console
+		for (int i = 1; i < arrayFinal.length; i++) {
+			
+			if (arrayFinal[i]>0)
 			{
-
-				if (valueArray[i]==0)
-				{
-					; //chill
-				}
-
-				if (valueArray[i]>0)
-				{
-					
-					
-					if (valueArray[i+1]==0)
-					{
-						System.out.print(valueArray[i]);
-					} else if (i != 0)
-					{
-						System.out.print("+");
-						System.out.print(valueArray[i]);
-					}
-
-				} else if (valueArray[i]<0){
-
-					System.out.print(valueArray[i]);
-
-				}
-
-			}
-
-
-			if (sum==0)
-			{
-				System.out.print("0");
+				System.out.print("+"+arrayFinal[i]);
 			} else
 			{
-				System.out.print(" = " + sum);
-				scanner.close();
+				System.out.print(arrayFinal[i]);
 			}
+			
+		}		
+		//print Sum
+		System.out.print("= " + sum);
 		}
 	}
-}
--- summingUpIntegerArray/src/main/java/de/hdm_stuttgart/mi/sd1/summingUpIntegerArray/killZeros.java
+++ summingUpIntegerArray/src/main/java/de/hdm_stuttgart/mi/sd1/summingUpIntegerArray/killZeros.java
@@ -9,17 +9,13 @@ public class killZeros {
 		for (int i = 0; i<a.length; i++)
 		{
 			if (a[i]==0)
+			// if the value of the array at index i == 0, count +1 to numberOfZeros .
 			{
 				numberOfZeros++;
 			}else
 			{
-				
+			// if the value of the array at index i !=0, do nothing.
 			}
-			
-//			if ((i-1)==a.length)
-//			{
-//				return numberOfZeros;
-//			}
 	}	
 		return numberOfZeros;
 }
